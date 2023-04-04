18d5f24d2f345ecbaff0377f484ee1c6e2101b56
ovan zaimul <ovanzaimul@gmail.com>
2023-04-04 14:41:08 +0800

    add exercise 2.2: Course information

--- part2/courseinfo/src/Total.js
+++ part2/courseinfo/src/Total.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Total = (props) => {
   const { parts } = props;
+  const total = parts.reduce((acc, curr) => acc + curr.exercises, 0);
   return (
     <p>
-      {`Number of exercises 
-     ${parts[0].exercises + parts[1].exercises + parts[2].exercises}`}
+      <b>{`total of ${total} exercises`}</b>
     </p>
   );
 };
