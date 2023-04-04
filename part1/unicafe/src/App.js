3acbb12dd97ac9d739b29edbb30967f6e3865b33
ovan zaimul <ovanzaimul@gmail.com>
2023-04-04 11:23:42 +0800

    add exercise 1.12*: anecdotes step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -23,6 +23,23 @@ const App = () => {
   const handleGoodClick = () => setGood(good + 1);
   const handleNeutralClick = () => setNeutral(neutral + 1);
   const handleBadClick = () => setBad(bad + 1);
+  const [selected, setSelected] = useState(0);
+
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const onAnecdoteClick = () => {
+    const randomNumber = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomNumber);
+  };
 
   return (
     <div>
@@ -47,6 +64,9 @@ const App = () => {
           <StatisticLine text="positive" value={`${(good / 9) * 100} %`} />
         </tbody>
       </table>
+
+      <p>{anecdotes[selected]}</p>
+      <button onClick={onAnecdoteClick}>next anecdote</button>
     </div>
   );
 };
